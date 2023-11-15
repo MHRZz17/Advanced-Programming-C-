ea8d8e22b15f319a8eeeb33c406abd17bd029d24
MohammadH.Rezaee <145509421+MHRZz17@users.noreply.github.com>
2023-11-15 03:42:22 +0330

    Add files via upload

---
+++ Avg_Height/Height/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Height
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button2 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.Window;
+            listBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 31;
+            listBox1.Location = new Point(397, 19);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(220, 376);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(85, 12);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.Yes;
+            label1.Size = new Size(224, 41);
+            label1.TabIndex = 1;
+            label1.Text = "قد به سانتی متر:";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 0, 0);
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(204, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 57);
+            button1.TabIndex = 2;
+            button1.Text = "ثبت";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(120, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 43);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(120, 328);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.Yes;
+            label2.Size = new Size(271, 41);
+            label2.TabIndex = 4;
+            label2.Text = "میانگین برابر است با:";
+            label2.Click += label2_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(192, 0, 0);
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(23, 169);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 57);
+            button2.TabIndex = 6;
+            button2.Text = "حذف همه";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(46, 328);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 41);
+            label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 128);
+            ClientSize = new Size(629, 407);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            RightToLeftLayout = true;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+        private Label label3;
+    }
+}
