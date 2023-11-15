2efac38afdf51818e6c5373fbd8aab98755fcd87
MohammadH.Rezaee <145509421+MHRZz17@users.noreply.github.com>
2023-11-15 03:41:14 +0330

    Add files via upload

---
+++ Customer_Panel/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(206, 9);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.Yes;
+            label1.Size = new Size(249, 62);
+            label1.TabIndex = 0;
+            label1.Text = "ثبت مشتری";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 0, 0);
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(55, 345);
+            button1.Name = "button1";
+            button1.Size = new Size(243, 76);
+            button1.TabIndex = 1;
+            button1.Text = "لیست مشتریان";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(192, 0, 0);
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(352, 345);
+            button2.Name = "button2";
+            button2.Size = new Size(243, 76);
+            button2.TabIndex = 2;
+            button2.Text = "ثبت مشتری جدید";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Window;
+            textBox3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(119, 250);
+            textBox3.Name = "textBox3";
+            textBox3.RightToLeft = RightToLeft.No;
+            textBox3.Size = new Size(264, 43);
+            textBox3.TabIndex = 16;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(407, 250);
+            label3.Name = "label3";
+            label3.Size = new Size(157, 41);
+            label3.TabIndex = 15;
+            label3.Text = "شماره تلفن";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Window;
+            textBox2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(119, 184);
+            textBox2.Name = "textBox2";
+            textBox2.RightToLeft = RightToLeft.Yes;
+            textBox2.Size = new Size(264, 43);
+            textBox2.TabIndex = 14;
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(119, 113);
+            textBox1.Name = "textBox1";
+            textBox1.RightToLeft = RightToLeft.Yes;
+            textBox1.Size = new Size(264, 43);
+            textBox1.TabIndex = 13;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(398, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(176, 41);
+            label4.TabIndex = 10;
+            label4.Text = "نام خانوادگی";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(458, 113);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 41);
+            label5.TabIndex = 9;
+            label5.Text = "نام";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 128);
+            ClientSize = new Size(646, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Customer_Panel/WinFormsApp2/Form1.cs
@@ -0,0 +1,53 @@
+﻿namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox1.Text;
+                string famil = textBox2.Text;
+                string tel = textBox3.Text;
+                string line = name + "," + famil + "," + tel + "\n";
+                File.AppendAllText("Customer.txt", line);
+                MessageBox.Show("ثبت با موفقیت انجام شد");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
